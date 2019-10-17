492ef1112f4a4d06d470e4b6e9e04224e91d7488
Jonathan Cruz <jcruzdeveloper@gmail.com>
2019-10-16 20:00:47 -0700

    Correct dates
    Account for utc timezone on heroku instance

--- README.md
+++ README.md
@@ -9,5 +9,5 @@ Query Parameters
 
 JSON Response [/api/earnings/AAPL?last=4](https://earningsapi.herokuapp.com/api/earnings/AAPL?last=4)
 ```json
-["2019-07-31","2019-05-01","2019-01-30","2018-11-02"]
+["2019-07-30","2019-04-30","2019-01-29","2018-11-01"]
 ```
--- app/services/optionslam.py
+++ app/services/optionslam.py
@@ -1,15 +1,23 @@
+import datetime
 import json
-from datetime import datetime
 from typing import List
 
+import pytz
 import requests
+
+NEW_YORK = pytz.timezone('America/New_York')
 
 
 def get_earnings_dates(stock_symbol: str, last: int = 4) -> List[str]:
     """Return earnings dates in format YYYY-MM-DD"""
+    dates: List[str] = []
     r = requests.get(f'https://www.optionslam.com/tv/marks?symbol={stock_symbol}')
     try:
         timestamps = r.json()['time'][:last]
     except (json.decoder.JSONDecodeError, KeyError):
-        return []
-    return [datetime.fromtimestamp(ts).strftime('%Y-%m-%d') for ts in timestamps]
+        return dates
+    for ts in timestamps:
+        dt = datetime.datetime.fromtimestamp(ts, tz=NEW_YORK)
+        dt -= datetime.timedelta(days=1)
+        dates.append(dt.strftime('%Y-%m-%d'))
+    return dates
